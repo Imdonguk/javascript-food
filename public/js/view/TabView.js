c468d1ae6117f8d4218c9823ac05f38f3184b41b
Imdonguk <com8157@naver.com>
2020-01-21 16:21:24 +0900

    Add : 리소스 url변경에 따른 적용

--- public/js/view/TabView.js
+++ public/js/view/TabView.js
@@ -1,69 +1,74 @@
-import { itemListTpl, tabListTpl } from '../template/itemListTpl.js';
-import { checkLocalItem, getLocalItem, pipe } from '../util.js'
+import { itemListTpl, tabListTpl } from "../template/itemListTpl.js";
+import { checkLocalItem, getLocalItem, pipe } from "../util.js";
 
 export default class TabView {
-    constructor({ bestSellerEl, urlRequestData }) {
-        this.bestEl = bestSellerEl;
-        this.bestTab = this.bestEl.querySelector('.best-seller-tab');
-        this.url = urlRequestData;
-        this.totalEvent();
-    }
+  constructor({ bestSellerEl, urlRequestData }) {
+    this.bestEl = bestSellerEl;
+    this.bestTab = this.bestEl.querySelector(".best-seller-tab");
+    this.url = urlRequestData;
+    this.totalEvent();
+  }
 
-    totalEvent() {
-        const event = {
-            click: () => {
-                this.bestTab.addEventListener('click', this.clickTabHandler.bind(this))
-            },
-            load: () => {
-                document.body.addEventListener('load', this.init.bind(this))
-            }
-        }
-        Object.keys(event).forEach(v => event[v]())
-    }
+  totalEvent() {
+    const event = {
+      click: () => {
+        this.bestTab.addEventListener("click", this.clickTabHandler.bind(this));
+      },
+      load: () => {
+        document.body.addEventListener("load", this.init.bind(this));
+      }
+    };
+    Object.keys(event).forEach(v => event[v]());
+  }
 
-    async clickTabHandler({ target }) {
-        if (target.tagName !== "LI") return;
-        if (await checkLocalItem(this.getUrl(this.getCategoryNo(target)))) return;
-        this.showTabContentUI(target);
-    }
+  async clickTabHandler({ target }) {
+    if (target.tagName !== "LI") return;
+    if (await checkLocalItem(this.getUrl(this.getCategoryNo(target)))) return;
+    this.showTabContentUI(target);
+  }
 
-    async init() {
-        if (await checkLocalItem(this.url)) return;
-        pipe(getLocalItem, tabListTpl, this.renderTab.bind(this))(this.url);
-        const firstNode = this.bestTab.firstElementChild;
-        if (await checkLocalItem(this.getUrl(this.getCategoryNo(firstNode)))) return;
-        this.showTabContentUI(firstNode);
-    }
+  async init() {
+    if (await checkLocalItem(this.url)) return;
+    pipe(getLocalItem, tabListTpl, this.renderTab.bind(this))(this.url);
+    const firstNode = this.bestTab.firstElementChild;
+    if (await checkLocalItem(this.getUrl(this.getCategoryNo(firstNode))))
+      return;
+    this.showTabContentUI(firstNode);
+  }
 
-    showTabContentUI(target) {
-        const url = this.getUrl(this.getCategoryNo(target))
-        this.blurTab();
-        this.focusTab(target);
-        pipe(getLocalItem, itemListTpl, this.renderContent.bind(this))(url);
-    }
+  showTabContentUI(target) {
+    const url = this.getUrl(this.getCategoryNo(target));
+    this.blurTab();
+    this.focusTab(target);
+    pipe(getLocalItem, itemListTpl, this.renderContent.bind(this))(url);
+  }
 
-    focusTab(target) {
-        target.classList.add('now');
-    }
+  focusTab(target) {
+    target.classList.add("now");
+  }
 
-    blurTab() {
-        const currentTab = this.bestEl.querySelector('.now')
-        currentTab && currentTab.classList.remove('now');
-    }
+  blurTab() {
+    const currentTab = this.bestEl.querySelector(".now");
+    currentTab && currentTab.classList.remove("now");
+  }
 
-    getUrl(category_no) {
-        return this.url + '/' + category_no;
-    }
+  getCategoryItem() {
+    getLocalItem(this.url).filter();
+  }
 
-    getCategoryNo(target) {
-        return target.getAttribute('category_no');
-    }
+  getUrl(category_no) {
+    return this.url + "/" + category_no;
+  }
 
-    renderContent(data) {
-        this.bestEl.querySelector('.best-list').innerHTML = data;
-    }
+  getCategoryNo(target) {
+    return target.getAttribute("category_no");
+  }
 
-    renderTab(data) {
-        this.bestTab.innerHTML = data;
-    }
+  renderContent(data) {
+    this.bestEl.querySelector(".best-list").innerHTML = data;
+  }
+
+  renderTab(data) {
+    this.bestTab.innerHTML = data;
+  }
 }
